1663284f3b545355eec7b796bfad47c3ddbc334e
Mahfuzur Rahman <mvr5854@psu.edu>
2024-11-16 22:25:35 -0500

    removed redundant function

--- src/Util/IOHelper.java
+++ src/Util/IOHelper.java
@@ -107,28 +107,4 @@ public class IOHelper {
         return parsedToDouble;
     }
 
-    /**
-     * Prompt for user input, and validate in range
-     * <p>
-     * Repeatedly prompt the user for a character.<p>
-     * Ensure that the character is within a range of characters
-     * <p>(which is packed into a string)
-     *
-     * @param prompt Prompt, so the user knows what to type in
-     * @param range  Valid characters allowed (e.g., "aeiou")
-     * @return In-range character chosen by the user
-     */
-    public static char userInputChar(String prompt, String range) {
-
-        char userChar = 0;
-        while (!ValidationHelper.isCharInRange(userChar, range)) {
-            String userInput = readStringFromKeyboard(prompt + " (" + range + ")");
-            if (userInput.length() > 0) {
-                userChar = userInput.charAt(0);
-            }
-        }
-
-        return userChar;
-    }
-
 }
