6dfbdc9371b6ae8da4c9fb055a98a8f2dadede19
Mahfuzur Rahman <mvr5854@psu.edu>
2024-11-24 00:02:38 -0500

    added more test for datastore class

--- unitTests/Model/DatastoreTest.java
+++ unitTests/Model/DatastoreTest.java
@@ -8,6 +8,44 @@ import static org.junit.jupiter.api.Assertions.*;
 class DatastoreTest {
 
     @Test
+    void getMovieById__pass_validId_return_correctMovie() {
+        Movie movie = Datastore.getMovieById(1);
+        assertEquals("Top Gun", movie.getTitle());
+    }
+
+    @Test
+    void getMovieById__pass_invalidId_return_null() {
+        Movie movie = Datastore.getMovieById(100);
+        assertNull(movie);
+    }
+
+    @Test
+    void searchMoviesByTitle__pass_unmatchedTitle_return_emptyList() {
+        List<Movie> foundMovies = Datastore.searchMoviesByTitle("Spiderman");
+        assertTrue(foundMovies.isEmpty());
+    }
+
+    @Test
+    void searchMoviesByTitle__pass_matchedTitle_return_correctMovies() {
+        List<Movie> foundMovies = Datastore.searchMoviesByTitle("Top");
+        assertTrue(foundMovies.size() == 1);
+        assertEquals("Top Gun", foundMovies.get(0).getTitle());
+    }
+
+    @Test
+    void getActorById__pass_validId_return_correctActor() {
+        Actor actor = Datastore.getActorById(1);
+        assertEquals("Tom", actor.getFirstName());
+        assertEquals("Cruise", actor.getLastName());
+    }
+
+    @Test
+    void getActorById__pass_invalidId_return_null() {
+        Actor actor = Datastore.getActorById(100);
+        assertNull(actor);
+    }
+
+    @Test
     void searchActorsByName__pass_unmatchedName_return_emptyList() {
         List<Actor> foundActor = Datastore.searchActorsByName("John");
         assertTrue(foundActor.isEmpty());
@@ -28,16 +66,15 @@ class DatastoreTest {
     }
 
     @Test
-    void searchMoviesByTitle__pass_unmatchedTitle_return_emptyList() {
-        List<Movie> foundMovies = Datastore.searchMoviesByTitle("Spiderman");
-        assertTrue(foundMovies.isEmpty());
+    void getAuditoriumById__pass_validId_return_correctAuditorium() {
+        Auditorium auditorium = Datastore.getAuditoriumById(1);
+        assertEquals("1", auditorium.toString());
     }
 
     @Test
-    void searchMoviesByTitle__pass_matchedTitle_return_correctMovies() {
-        List<Movie> foundMovies = Datastore.searchMoviesByTitle("Top");
-        assertTrue(foundMovies.size() == 1);
-        assertEquals("Top Gun", foundMovies.get(0).getTitle());
+    void getAuditoriumById__pass_invalidId_return_null() {
+        Auditorium auditorium = Datastore.getAuditoriumById(100);
+        assertNull(auditorium);
     }
 
     @Test
@@ -51,5 +88,30 @@ class DatastoreTest {
         List<Theater> foundTheaters = Datastore.searchTheatersByName("AMC");
         assertEquals(1, foundTheaters.size());
         assertTrue(foundTheaters.get(0).getName().contains("AMC"));
+    }
+
+    @Test
+    void searchTheatersByZipcode__pass_unmatchedZipcode_return_emptyList() {
+        List<Theater> foundTheaters = Datastore.searchTheatersByZipcode("12345");
+        assertTrue(foundTheaters.isEmpty());
+    }
+
+    @Test
+    void searchTheatersByZipcode__pass_matchedZipcode_return_correctTheaters() {
+        List<Theater> foundTheaters = Datastore.searchTheatersByZipcode("19047");
+        assertEquals(1, foundTheaters.size());
+        assertTrue(foundTheaters.get(0).getName().contains("Regal"));
+    }
+
+    @Test
+    void getShowingById__pass_validId_return_correctShowing() {
+        Showing showing = Datastore.getShowingById(1);
+        assertTrue(showing.toString().contains("Top Gun"));
+    }
+
+    @Test
+    void getShowingById__pass_invalidId_return_null() {
+        Showing showing = Datastore.getShowingById(100);
+        assertNull(showing);
     }
 }
