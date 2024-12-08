a372fc66cf68f269841f2ac02d035dd1906d06f8
Mahfuzur Rahman <mvr5854@psu.edu>
2024-12-08 13:51:54 -0500

    Added more IoHelper tests

--- src/test/java/edu/psu/fauxdango/Util/IOHelperTest.java
+++ src/test/java/edu/psu/fauxdango/Util/IOHelperTest.java
@@ -1,9 +1,5 @@
 package edu.psu.fauxdango.Util;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
@@ -12,23 +8,18 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
 
+import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class IOHelperTest {
     private ByteArrayOutputStream consoleOut;
-    private InputStream originalConsoleIn;
 
     @BeforeEach
     void setUp() {
-        // set current system.in to originalConsoleIn
-        originalConsoleIn = System.in;
         consoleOut = new ByteArrayOutputStream();
         System.setOut(new PrintStream(consoleOut));
-    }
-
-    @AfterEach
-    void tearDown() {
-        System.setIn(originalConsoleIn);
     }
 
     void setInputStream(String input) {
@@ -47,22 +38,44 @@ class IOHelperTest {
     }
 
     @Test
+    @Order(1)
     void readValidNameFromKeyboard__pass_validName__return_name() {
-        setInputStream("John\n");
+        setInputStream("John\nalpha45\nJohn\njohn@psu.edu\nte$t@email.me\njohn@psu.edu\n");
         String result = IOHelper.readValidNameFromKeyboard("Enter your name");
         assertEquals("John", result);
 
     }
 
     @Test
+    @Order(2)
     void readValidNameFromKeyboard__pass_invalidName__prompt_again() {
-        setInputStream("alpha45\nJohn\nAdib");
+        setInputStream("alpha45\nJohn\n");
         String result = IOHelper.readValidNameFromKeyboard("Enter your name");
         String output = consoleOut.toString().replaceAll("\r", "");
 
         assertEquals("John", result);
         assertTrue(output.contains("Name can only contain letters\nand must begin with a capital letter"));
     }
+
+    @Test
+    @Order(3)
+    void readValidEmailAddressFromKeyboard__pass_validEmail__return_email() {
+        setInputStream("john@psu.edu\n");
+        String result = IOHelper.readValidEmailAddressFromKeyboard("Enter your email address");
+        assertEquals("john@psu.edu", result);
+    }
+
+    @Test
+    @Order(4)
+    void readValidEmailAddressFromKeyboard__pass_invalidEmail__return_email() {
+        setInputStream("te$t@email.me\njohn@psu.edu\n");
+        String result = IOHelper.readValidEmailAddressFromKeyboard("Enter your email address");
+        String output = consoleOut.toString().replaceAll("\r", "");
+
+        assertTrue(output.contains("Invalid email address! try again"));
+        assertEquals("john@psu.edu", result);
+    }
+
 
     @Test
     void checkRegex__pass_validString_matchedPattern__return_true() {
