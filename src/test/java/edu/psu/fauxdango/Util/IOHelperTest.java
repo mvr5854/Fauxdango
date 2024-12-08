aa07bb5038f19436baffa05136acc95ad4e85510
Mahfuzur Rahman <mvr5854@psu.edu>
2024-12-08 17:20:06 -0500

    Updated IoHelper tests

--- src/test/java/edu/psu/fauxdango/Util/IOHelperTest.java
+++ src/test/java/edu/psu/fauxdango/Util/IOHelperTest.java
@@ -1,9 +1,6 @@
 package edu.psu.fauxdango.Util;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.PrintStream;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
@@ -12,22 +9,9 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class IOHelperTest {
-    private ByteArrayOutputStream consoleOut;
-
-    @BeforeEach
-    void setUp() {
-        consoleOut = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(consoleOut));
-    }
-
-    void setInputStream(String input) {
-        System.setIn(new ByteArrayInputStream(input.getBytes()));
-    }
-
     @Test
-    void testSingleton__pass_none__return_onlyOneStaticMethod() {
+    void testSingleton__get_allStaticMethod__return_onlyOneStaticMethod() {
         Method[] methods = IOHelper.class.getDeclaredMethods();
         Method[] allStaticMethods = Arrays.stream(methods)
                 .filter(method -> Modifier.isStatic(method.getModifiers()))
@@ -38,44 +22,23 @@ class IOHelperTest {
     }
 
     @Test
-    @Order(1)
-    void readValidNameFromKeyboard__pass_validName__return_name() {
-        setInputStream("John\nalpha45\nJohn\njohn@psu.edu\nte$t@email.me\njohn@psu.edu\n");
-        String result = IOHelper.readValidNameFromKeyboard("Enter your name");
-        assertEquals("John", result);
-
+    void testSingleton__get_constructorModifier__return_privateModifier() {
+        Constructor<IOHelper> constructor = null;
+        try {
+            constructor = IOHelper.class.getDeclaredConstructor();
+        } catch (NoSuchMethodException e) {
+            fail("Constructor not found");
+        }
+        assertNotNull(constructor);
+        assertTrue(Modifier.isPrivate(constructor.getModifiers()));
     }
 
     @Test
-    @Order(2)
-    void readValidNameFromKeyboard__pass_invalidName__prompt_again() {
-        setInputStream("alpha45\nJohn\n");
-        String result = IOHelper.readValidNameFromKeyboard("Enter your name");
-        String output = consoleOut.toString().replaceAll("\r", "");
-
-        assertEquals("John", result);
-        assertTrue(output.contains("Name can only contain letters\nand must begin with a capital letter"));
+    void testSingleton__create_multipleInstance__return_sameInstance() {
+        IOHelper instance1 = IOHelper.getInstance();
+        IOHelper instance2 = IOHelper.getInstance();
+        assertSame(instance1, instance2);
     }
-
-    @Test
-    @Order(3)
-    void readValidEmailAddressFromKeyboard__pass_validEmail__return_email() {
-        setInputStream("john@psu.edu\n");
-        String result = IOHelper.readValidEmailAddressFromKeyboard("Enter your email address");
-        assertEquals("john@psu.edu", result);
-    }
-
-    @Test
-    @Order(4)
-    void readValidEmailAddressFromKeyboard__pass_invalidEmail__return_email() {
-        setInputStream("te$t@email.me\njohn@psu.edu\n");
-        String result = IOHelper.readValidEmailAddressFromKeyboard("Enter your email address");
-        String output = consoleOut.toString().replaceAll("\r", "");
-
-        assertTrue(output.contains("Invalid email address! try again"));
-        assertEquals("john@psu.edu", result);
-    }
-
 
     @Test
     void checkRegex__pass_validString_matchedPattern__return_true() {
